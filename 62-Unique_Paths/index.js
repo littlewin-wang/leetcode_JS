aa0bf8e771778eed3652b0fdb23c904af332e065
littlewin <littlewin.wang@sky-cloud.net>
2019-03-21 11:58:54 +0800

    add(62): unique paths

---
+++ 62-Unique_Paths/index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  var dp = []
+  for (var i = 0; i < m; i++) {
+    dp.push(new Array(n).fill(0))
+  }
+
+  for (var i = 0; i < m; i++) {
+    for (var j = 0; j < n; j++) {
+      if (i == 0 || j == 0) {
+        dp[i][j] = 1
+      }
+      else {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+      }
+    }
+  }
+  return dp[m - 1][n - 1]
+}
